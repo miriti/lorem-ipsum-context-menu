24354a817828f3822ef7c0eec708aaf8deab7548
Michael Miriti <kefir.rwr@gmail.com>
2013-09-18 17:50:32 +0400

    annoying console.log

--- src/content.js
+++ src/content.js
@@ -2,6 +2,4 @@ function onMessage(request, sender, sendResponse) {
    $(':focus').val( $(':focus').val() + request.text );
 }
 
-console.log('hi, i am here again!!');
-
 chrome.runtime.onMessage.addListener(onMessage);
